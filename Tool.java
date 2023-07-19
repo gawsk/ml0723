9cdc231b4df152904cf573586ca39a08b59d82fa
Morgan Larkin <16796263+gawsk@users.noreply.github.com>
2023-07-19 18:04:42 -0400

    Initial classes

---
+++ Tool.java
@@ -0,0 +1,5 @@
+public class Tool {
+    private String toolCode;
+    private ToolType toolType;
+    private String brand;
+}
---
+++ ToolType.java
@@ -0,0 +1,7 @@
+public class ToolType {
+    private String name;
+    private float dailyCharge;
+    private boolean weekdayCharge;
+    private boolean weekendCharge;
+    private boolean holidayCharge;
+}
